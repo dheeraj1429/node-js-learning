0bcfc0125ce61ba3cbfca301390863c0e1214010
dheeraj1429 <dheerajsingh1429@gmail.com>
2022-05-18 23:14:20 +0530

    learning ndoe js

---
+++ GRAPH-QL/graphql/productsSchema/products.modle.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id: 1,
+        posterUrl: "https://m.media-amazon.com/images/I/71X8NdnCsvL._SX522_.jpg",
+        Discription:
+            "Mi Smart Band 5 – India’s No. 1 Fitness Band, 1.1-inch AMOLED Color Display, Magnetic Charging, 2 Weeks Battery Life, Personal Activity Intelligence (PAI), Women’s Health Tracking",
+        shortTitle: "Mi Smart Band 5",
+        ragulerPrice: 3000,
+        Deal_of_the_Day: 1999,
+        rating: " 51,888 ratings",
+        anwsered: "1000+ answered questions",
+        offer: "Great Indian Festival",
+
+        shortImages: [
+            { url: "https://m.media-amazon.com/images/I/71jny5ULL8L._SL1500_.jpg" },
+            { url: "https://m.media-amazon.com/images/I/71H0UwhNi3L._SL1500_.jpg" },
+            { url: "https://m.media-amazon.com/images/I/61hL0gam1ML._SX522_.jpg" },
+            { url: "https://m.media-amazon.com/images/I/81Kw2Z2SUFL._SX522_.jpg" },
+        ],
+        About: `1.1” Full touch AMOLED color display
+        Battery runs up to 14 days on a single charge. Battery capacity : 125 mAh
+        Magnetic charging – Hassle free charging
+        PAI (Personal Activity Intelligence) – Single matrix to track your all fitness related activities.
+        Tracks 11 professional sports mode (including Yoga and Rope skipping). Run on the go with Automatic activity detection (Running and Walking).
+        5ATM Water Resistant- recognizes swimming mode.
+        Stress monitoring with Guided breathing exercise to lower the stress level. Women health monitoring- menstrual cycle tracking and notification.
+        24X7 Sleep monitoring along with REM (Rapid Eye Movement) Sleep monitoring.
+        Smart Features: App notifications, control music, manage your calls, check Weather, alarm, idle alert, locate your phone etc.
+        Unlimited Watch faces.
+        Compatible with android 5.0 and above and ios 10 and above.`,
+        reviews: [
+            {
+                userId: 'objectId("123123u981y293y123123123")',
+                rating: "5 starts",
+                commnet: "best products in market",
+            },
+            {
+                userId: 'objectId("1231238@HJKGJ@123")',
+                rating: "3 starts",
+                commnet: "goot products in market",
+            },
+            {
+                userId: 'objectId("1233y123123123")',
+                rating: "4 starts",
+                commnet: "some demo data",
+            },
+        ],
+    },
+];
